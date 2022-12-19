b190832d74fceb003f086cdbb10319812f503b95
Bruce Baynosa <bruce.baynosa@asggroup.com.au>
2022-12-19 20:47:17 +0800

    Testing pipeline

--- todos/get.py
+++ todos/get.py
@@ -10,16 +10,22 @@ table = dynamodb.Table(table_name)
 
 
 def handler(event, context):
-    id = event["pathParameters"]['id']
-    res = table.query(
-        KeyConditionExpression=Key('id').eq(id)
-    )
-    todo = res["Items"][0]
+    # id = event["pathParameters"]['id']
+    # res = table.query(
+    #     KeyConditionExpression=Key('id').eq(id)
+    # )
+    # todo = res["Items"][0]
+
+    # response = {"statusCode": 200, "headers": {
+    #     "Access-Control-Allow-Headers": "*",
+    #     "Access-Control-Allow-Origin": "*",
+    #     "Access-Control-Allow-Methods": "*"
+    # }, "body": json.dumps(todo)}
 
     response = {"statusCode": 200, "headers": {
         "Access-Control-Allow-Headers": "*",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "*"
-    }, "body": json.dumps(todo)}
+    }, "body": json.dumps("Hello sls pipelines!")}
 
     return response
