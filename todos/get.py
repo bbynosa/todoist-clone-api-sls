eb02156ce8c2495c5319101b8662559972412032
Bruce Baynosa <bruce.baynosa@asggroup.com.au>
2022-12-19 20:51:07 +0800

    Revert previous code after pipeline test

--- todos/get.py
+++ todos/get.py
@@ -10,22 +10,16 @@ table = dynamodb.Table(table_name)
 
 
 def handler(event, context):
-    # id = event["pathParameters"]['id']
-    # res = table.query(
-    #     KeyConditionExpression=Key('id').eq(id)
-    # )
-    # todo = res["Items"][0]
-
-    # response = {"statusCode": 200, "headers": {
-    #     "Access-Control-Allow-Headers": "*",
-    #     "Access-Control-Allow-Origin": "*",
-    #     "Access-Control-Allow-Methods": "*"
-    # }, "body": json.dumps(todo)}
+    id = event["pathParameters"]['id']
+    res = table.query(
+        KeyConditionExpression=Key('id').eq(id)
+    )
+    todo = res["Items"][0]
 
     response = {"statusCode": 200, "headers": {
         "Access-Control-Allow-Headers": "*",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "*"
-    }, "body": json.dumps("Hello sls pipelines!")}
+    }, "body": json.dumps(todo)}
 
     return response
